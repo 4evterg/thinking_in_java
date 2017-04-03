3f1a0f59473225d83ad11bc8cbde943833973647
4evterg <4evterg@gmail.com>
2017-04-03 18:44:09 +0300

    Continue play with inner classes. Getting access to multinested classes :3

---
+++ java_learn_1/src/com/chetverg/java/learn/MultiNestingAccess.java
@@ -0,0 +1,36 @@
+package com.chetverg.java.learn;
+
+import static com.chetverg.java.learn.Print.print;
+
+/**
+ * Created by chetverg on 03.04.17.
+ */
+
+class MNA{
+    private void a(){
+        print("first level");
+    }
+    class A{
+        private void b(){
+            print("second level");
+        }
+        public class B{
+            void c(){
+                a();
+                b();
+            }
+
+        }
+    }
+}
+
+public class MultiNestingAccess {
+    public static class Tester{
+        public static void main(String[] args) {
+            MNA mna = new MNA();
+            MNA.A mnaa = mna.new A();
+            MNA.A.B mnaab = mnaa.new B();
+            mnaab.c();
+        }
+    }
+}
